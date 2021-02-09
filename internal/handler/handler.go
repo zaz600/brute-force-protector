21958fc9790f5b9fc8fc8578f637cf0b3efd62e5
Dmitriy Denisov <zaz600@gmail.com>
2021-02-10 00:34:19 +0300

    add linebreaks

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -14,7 +14,7 @@ type Server struct {
 }
 
 func (s *Server) Verify(ctx context.Context, req *protectorpb.VerifyRequest) (*protectorpb.VerifyResponse, error) {
-	log.Printf("verify with params: %v", req)
+	log.Printf("verify with params: %v\n", req)
 	ip := req.VerifyParams.GetIp()
 	login := req.VerifyParams.GetLogin()
 	password := req.VerifyParams.GetPassword()
@@ -22,44 +22,44 @@ func (s *Server) Verify(ctx context.Context, req *protectorpb.VerifyRequest) (*p
 	resp := &protectorpb.VerifyResponse{
 		Ok: s.protector.Verify(ctx, login, password, ip),
 	}
-	log.Printf("verify result: %#v", resp.Ok)
+	log.Printf("verify result: %#v\n", resp.Ok)
 	return resp, nil
 }
 
 func (s *Server) ResetLogin(ctx context.Context, req *protectorpb.ResetLoginLimitRequest) (*protectorpb.ResetLoginLimitResponse, error) {
-	log.Printf("ResetLogin with params: %v", req)
+	log.Printf("ResetLogin with params: %v\n", req)
 	s.protector.ResetLogin(ctx, req.GetLogin())
 	return &protectorpb.ResetLoginLimitResponse{}, nil
 }
 
 func (s *Server) ResetIP(ctx context.Context, req *protectorpb.ResetIPLimitRequest) (*protectorpb.ResetIPLimitResponse, error) {
-	log.Printf("ResetIP with params: %v", req)
+	log.Printf("ResetIP with params: %v\n", req)
 	s.protector.ResetLogin(ctx, req.GetLogin())
 	return &protectorpb.ResetIPLimitResponse{}, nil
 }
 
 func (s *Server) AddBlackList(ctx context.Context, req *protectorpb.AddBlackListRequest) (*protectorpb.AddBlackListResponse, error) {
 	// TODO логировать ошибку или вернуть ее в ответ
-	log.Printf("AddBlackList with params: %v", req)
+	log.Printf("AddBlackList with params: %v\n", req)
 	_ = s.protector.AddBlackList(ctx, req.NetworkCIDR)
 	return &protectorpb.AddBlackListResponse{}, nil
 }
 
 func (s *Server) RemoveBlackList(ctx context.Context, req *protectorpb.RemoveBlackListRequest) (*protectorpb.RemoveBlackListResponse, error) {
-	log.Printf("RemoveBlackList with params: %v", req)
+	log.Printf("RemoveBlackList with params: %v\n", req)
 	s.protector.RemoveBlackList(ctx, req.NetworkCIDR)
 	return &protectorpb.RemoveBlackListResponse{}, nil
 }
 
 func (s *Server) AddWhiteList(ctx context.Context, req *protectorpb.AddWhiteListRequest) (*protectorpb.AddWhiteListResponse, error) {
 	// TODO логировать ошибку или вернуть ее в ответ
-	log.Printf("AddWhiteList with params: %v", req)
+	log.Printf("AddWhiteList with params: %v\n", req)
 	_ = s.protector.AddBlackList(ctx, req.NetworkCIDR)
 	return &protectorpb.AddWhiteListResponse{}, nil
 }
 
 func (s *Server) RemoveWhiteList(ctx context.Context, req *protectorpb.RemoveWhiteListRequest) (*protectorpb.RemoveWhiteListResponse, error) {
-	log.Printf("RemoveWhiteList with params: %v", req)
+	log.Printf("RemoveWhiteList with params: %v\n", req)
 	s.protector.RemoveBlackList(ctx, req.NetworkCIDR)
 	return &protectorpb.RemoveWhiteListResponse{}, nil
 }
